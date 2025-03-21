a9dd047dfdf3dc117aae151e92aa1ce7b2e0bd7d
Thassiane lima <thassiane321@hotmail.com>
2025-03-21 20:49:10 -0300

    update

---
+++ dio 2/index24.js
@@ -0,0 +1,10 @@
+function torrar(){
+    console.log('tostando pão...')
+}
+torrar()
+
+function injetarPao(){
+    console.log('preparando para injetar o pão...')
+    console.log('finalizado')
+}
+injetarPao()
---
+++ dio 2/index25.js
@@ -0,0 +1,25 @@
+function mainSaveData(){
+    getData()
+    checkValues()
+    sendToDataBase()
+}
+
+function getData(){
+    //get data from here
+    console.log('pegando dados do usuario')
+    if (1 < 3){
+        console.log('numero encontrado')
+    }
+}
+
+function checkValues(){
+    //check values here
+    console.log('validando dados do usuario')
+}
+
+function sendToDataBase(){
+    //send to database here
+    console.log('enviando dados para o banco de dados')
+}
+
+mainSaveData()
